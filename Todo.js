d4e460c9fe53dd79dee70345993c5bf937e97da4
BijayKumarNayak <91744852+BijayKumarNayak@users.noreply.github.com>
2023-03-07 22:28:00 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -2,24 +2,20 @@ import React from "react";
 import { useState } from "react";
 // import { ReactDOM } from "react";
 import "./todo.css";
-import Button from "@mui/material/Button";
+
 
 function Todo() {
   const [todos, setTodos] = useState("");
   const [items, setItems] = useState([]);
 
   const additem = () => {
-    if (todos.length===0){
-      alert(" opps! please Enter todo")
-      
-    }
-  
-    
-   else setItems((oldvalue) => {
-      return [...oldvalue, todos];
-    });
-   setTodos("")
-   
+    if (todos.length === 0) {
+      alert(" opps! please Enter todo");
+    } else
+      setItems((oldvalue) => {
+        return [...oldvalue, todos];
+      });
+    setTodos("");
   };
   const remove = (id) => {
     console.log("removed");
@@ -31,47 +27,35 @@ function Todo() {
   };
 
   return (
-    <div id="todo">
-      <i>
-        <h2>Todo List</h2>
-      </i>
-      <form>
-        <input
-          type="text"
-          placeholder="Enter Todo"
-          value={todos}
-          onChange={(e) => setTodos(e.target.value)}
-          required
-        />
+    <div id="main">
+    
+      <div id="todo">
+       <h1>Todo List</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Enter Todo"
+            value={todos}
+            onChange={(e) => setTodos(e.target.value)}
+            required
+          />
 
-        <Button className="btn" variant="contained" onClick={additem}>
-          Add
-        </Button>
-      </form>
-      <div id="output">
-        <ul >
-          {items.map((item, ind) => {
-            return (
-              <div className="render">
-                <li key={ind}>
-                  {item}
-                </li>
-                <Button
-                  variant="contained"
-                  color="error"
-                  
-                  className="btn"
-                  onClick={() => remove(ind)}
-                >
-                  Remove
-                </Button>
+          
+          <button onClick={additem} className="addbtn">Add</button>
+        </form>
+        <div id="output">
+          <ul>
+            {items.map((item, ind) => {
+              return (
+                <div className="render">
+                  <li key={ind}>{item}</li>
+                 <button className="removebtn" onClick={() => remove(ind)}>Remove</button>
                 </div>
-             
-             
-          );
-        })}
-      </ul>
-    </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
